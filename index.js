5a5ae09af524e1703da805b6b91dc66a676fb128
adrian.ducuara <adriandesarrollador@outlook.com>
2024-04-19 23:41:57 -0500

    fix: add improvements about imports

---
+++ index.js
@@ -0,0 +1,5 @@
+import './components/dm/pokeApiDm';
+import './components/ui/listComponent/listComponent';
+import './components/ui/headerComponent/headerComponent';
+import './components/ui/detailComponent/detailComponent';
+import './components/ui/formComponent/formComponent';
